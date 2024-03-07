8ab55682c778c22e10c66bec88a4978410230647
DanteDeLordran <dantelopez305@outlook.com>
2024-03-07 16:13:46 -0600

    List , Set , Map , For and ForEach

--- JavaMaster.java
+++ JavaMaster.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class JavaMaster {
@@ -15,14 +17,41 @@ public class JavaMaster {
         List<Integer> numbersList = new ArrayList<>();
         numbersList.add(1);
         numbersList.remove(numbersList.indexOf(1));
-        int listSize = numbersList.size();
+        int size = numbersList.size();
+        List<Integer> anotherList = new ArrayList<>();
+        numbersList.addAll(anotherList);
+        numbersList.clear();
 
         //Set
-        Set<Integer> numbersSet = new HashSet<>();
-        numbersSet.add(1);
-        numbersSet.remove(1);
-
+        Set<Integer> someSet = new HashSet<>();
+        someSet.add(1);
+        someSet.add(2);
+        someSet.add(2);
+        boolean belongs = someSet.contains(7);
         
+        //Map
+        Map<String, int[]> studentScores = new HashMap<>();
+        studentScores.put("dante", new int[]{100, 100, 100});
+        studentScores.put("blanca", new int[]{100, 100, 100});
 
+        //Ternary operator
+        String message = true ? "Te amo blanca" : "Como quiera te amo";
+
+        //For each 
+        String[] nameList = {"Dante", "Blanca"};
+        for (String name : nameList) {
+            System.out.println(name);
+        }
+
+        //For (Python for in range)
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Tas bien chula bb");
+        }
+
+        //(For enum in Python)
+        String[] anotherNameList = {"Dante", "Blanca"};
+        for (int i = 0; i < anotherNameList.length; i++) {
+            System.out.println(anotherNameList[i]);
+        }
     }
 }
--- python_master.py
+++ python_master.py
@@ -1,1 +1,44 @@
 print('Te amo Blanca')
+
+# Tuple
+number_tuple = (1,2,3,3,3,3)
+
+# List
+number_list = []
+number_list.append(1)
+number_list.remove(number_list.index(1))
+size = len(number_list)
+another_list = [2,2,2,2]
+number_list.extend(another_list)
+number_list.clear()
+
+# Set
+some_set = set([1,2,2,2,2,2])
+belongs = 7 in some_set
+
+# Frozen Set
+fruits = { 'apple' , 'watermelon' }
+fruit_frozen_set = frozenset(fruits)
+
+# Dictionary (Java map)
+student_scores = {
+    'dante' : [100,100,100],
+    'blanca' : [100,100,100]
+}
+
+# Ternary operator
+message = 'Te amo blanca' if True else 'Como quiera te amo'
+
+# For each
+name_list = ['Dante' , 'Blanca']
+for name in name_list:
+    print(name)
+    
+# For in range (Java for)
+for i in range(5):
+    print('Tas bien chula bb')
+    
+# For enum
+another_name_list = ['Dante' , 'Blanca']
+for i,value in enumerate(another_name_list):
+    print(another_name_list[i])
