7615f2fcf3780da8e28aa8f4ae42a3c3ca06ecb2
Dante de Lordran <98248751+DanteDeLordran@users.noreply.github.com>
2024-06-04 11:34:52 -0600

    Update python_master.py

--- python_master.py
+++ python_master.py
@@ -1,11 +1,10 @@
 from functools import reduce
 
-print('Te amo Blanca')
-
-# Tuple
+############## Tuple ###############
+# Inmutable
 number_tuple = (1,2,3,3,3,3)
 
-# List
+############## List ###############
 number_list = []
 number_list.append(1)
 number_list.remove(number_list.index(1))
@@ -14,52 +13,47 @@ another_list = [2,2,2,2]
 number_list.extend(another_list)
 number_list.clear()
 
-# Set
+############## Set ###############
 some_set = set([1,2,2,2,2,2])
 belongs = 7 in some_set
 
-# Frozen Set
+############## Frozen set ###############
 fruits = { 'apple' , 'watermelon' }
 fruit_frozen_set = frozenset(fruits)
 
-# Dictionary (Java map)
+############## Dictionary ###############
 student_scores = {
     'dante' : [100,100,100],
     'blanca' : [100,100,100]
 }
 
-# Ternary operator
+############## Ternary operator ###############
 message = 'Te amo blanca' if True else 'Como quiera te amo'
 
-# For each
+############## Foreach ###############
 name_list = ['Dante' , 'Blanca']
 for name in name_list:
     print(name)
-    
-# For in range (Java for)
+
+############## For ###############
 for i in range(5):
     print('Tas bien chula bb')
     
-# For enum
+############## For enum ###############
 another_name_list = ['Dante' , 'Blanca']
 for i,value in enumerate(another_name_list):
     print(another_name_list[i])
-    
-def some_func():
-    print('Do something')
-    
-def another_func(first , second):
-    print(first , second)
-    
-another_func(second='a', first=1)
 
+############## Collection MAP ###############
 # Higher order function MAP
 name_list_uppercase = list(map(str.upper(), name_list))
 
+############## Collection FILTER ###############
 # Higher order function FILTER
 another_number_list = [1,2,3,3,4,7,0,5,5]
 is_odd = list(filter( lambda n: n%2==0 , another_number_list ))
 
+############## Collection SORT ###############
 # Higher order function SORT
 students = [
     ('Dante', 22, 100),
@@ -71,32 +65,14 @@ sorted_list_students = list(sorted( lambda x: x[1] ,students ))
 numbers = [6,6,6,6,6,6,6,6]
 total = reduce( lambda x,y : (x + y) * 2 , numbers)
 
-# Classes
+############## Class ###############
 class User:
     def __init__(self, name, password):
         self.__name = name
         self.__password = password
 
-    @property
-    def name(self):
-        return self.__name
 
-    @name.setter
-    def name(self, name):
-        self.__name = name
-
-    @property
-    def password(self):
-        return self.__password
-
-    @password.setter
-    def password(self, password):
-        self.__password = password
-
-    def say_something(self):
-        return "I hate communists"
-    
-# Inherence
+############## Inheritance ###############
 class Student(User):
     def __init__(self, name, password, registration):
         super().__init__(name, password)
@@ -109,3 +85,22 @@ class Student(User):
     @registration.setter
     def registration(self, registration):
         self._registration = registration
+
+
+############## List comprehention ###############
+numbers = [1,2,3,4,5]
+
+numbrers_sqrts = []
+for n in numbers:
+    numbrers_sqrts.append(n**2)
+
+sqrts = [n**2 for n in numbers if n%2 == 0]
+
+matrix = []
+for _ in range(3):
+    intern_list = []
+    for i in range(3):
+        intern_list.append(i)
+    matrix.append(intern_list)
+
+matrix2 = [[i for i in range(3)] for _ in range(3)]
